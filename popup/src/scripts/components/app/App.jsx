3e4a22febe67a08a86db238b956cad018b97a18f
Adam Banko <banko.adam@gmail.com>
2018-06-09 12:23:54 +0200

    added ESLint

--- popup/src/scripts/components/app/App.jsx
+++ popup/src/scripts/components/app/App.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import {Component} from 'react'
+import {connect} from 'react-redux'
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +25,7 @@ class App extends Component {
             {this.getTimeLimit()} seconds limit
           </div>
           :
-          <div style={{width: 200, height: 100, display: 'flex', flexDirection: "column", alignItems: "flex-end", justifyContent: "space-evenly"}}>
+          <div style={{width: 200, height: 100, display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'space-evenly'}}>
             <div>
               <button style={{fontSize: 20}} onClick={() => this.props.startAction(this.getTimeLimit())}>
                 Start
@@ -35,9 +35,9 @@ class App extends Component {
               with
               <input
                 type="text"
-                style={{"width": 35, fontSize: 20, margin: 5}}
+                style={{'width': 35, fontSize: 20, margin: 5}}
                 value={this.getTimeLimit()}
-                onChange={(e) => (this.setState({_timeLimit: e.target.value}))}
+                onChange={(e) => this.setState({_timeLimit: e.target.value})}
               />
               seconds limit
             </div>
@@ -61,7 +61,7 @@ function startAction(timeLimit) {
 
 function stopAction() {
   return {
-    type: 'STOP_TRACKING',
+    type: 'STOP_TRACKING'
   }
 }
 
@@ -69,13 +69,13 @@ const mapStateToProps = (state) => {
   return {
     state: state,
     tracking: state.tracking,
-    defaultTimeLimit: state.settings && state.settings.timeLimit || 0,
+    defaultTimeLimit: state.settings && state.settings.timeLimit || 0
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
   startAction: (timeLimit) => dispatch(startAction(timeLimit)),
-  stopAction: () => dispatch(stopAction()),
+  stopAction: () => dispatch(stopAction())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
